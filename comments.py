8ed5de0c3d3de0658840b742ee3960857b66e957
김자영 <1476532@sookmyung.ac.kr>
2020-01-25 02:30:43 +0900

    필요 없는 라인 정리, 주석 추가

--- comments.py
+++ comments.py
@@ -3,14 +3,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import pymysql 
-import time
-from selenium.webdriver.common.alert import Alert
-from selenium.common.exceptions import NoAlertPresentException
+import astropy.io.misc.asdf.tags.time.tests.test_time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 # create cursor
-
 conn = pymysql.connect(host = "127.0.0.1",
                        user = 'root', passwd = '132365', db = 'gradproj')
 cur = conn.cursor()
@@ -34,15 +31,19 @@ bs = BeautifulSoup(source, 'html.parser')
 # 페이지 옆에 다음 버튼 계속 누름. 마지막 페이지라는 alert 뜰 때까지
 # alert 만나면 확인버튼 클릭 후 False 리턴
 def change_pages():
+    # '다음 페이지' 버튼 찾기
     temp = driver.find_element_by_class_name("page-nav")
     button_pages = temp.find_element_by_class_name("next")  
 
+    # 버튼 클릭
     button_pages.click()
     
+    # 마지막 페이지일 경우 뜨는 alert 제어 
     try:
         alert = Alert(driver)
         alert.accept()
 
+    # 마지막 페이지 아닌 경우 
     except NoAlertPresentException:
         return True
 
@@ -53,13 +54,15 @@ def change_songs(song_num):
     source = driver.page_source
     bs = BeautifulSoup(source, 'html.parser')
     
+    # 첫 번째 곡부터 순서대로 버튼 찾아 선택
     xpath_songs = "html/body/div[3]/div[2]/div[1]/div[6]/div/table/tbody/tr[" + str(song_num) + "]/td[4]/a"
     button_songs = driver.find_element_by_xpath(xpath_songs)
     button_songs.click()
 
+    # 아이피 차단 방지용
     time.sleep(1)
 
-    # crawl page 1
+    # 댓글 1페이지 크롤링
     crawlComments(bs)
 
     while(change_pages()):
@@ -68,9 +71,11 @@ def change_songs(song_num):
 
         crawlComments(bs)
         
+    # 한 곡의 댓글 모두 크롤링한 후 메인페이지로 돌아감
     driver.get('https://genie.co.kr/chart/top200')
     source = driver.page_source
 
+# 댓글 크롤링하는 함수
 def crawlComments(bs):
     replies = bs.find_all(class_="reply-text")
 
@@ -84,9 +89,13 @@ def crawlComments(bs):
         """%( str(writerId[i]), str(comment[i]) )
             
         cur.execute(query)
-    time.sleep(1)
 
+    time.sleep(1)
+    # conn.commit()
+
+# 1위부터 100위까지 100곡 크롤링할 것 
 for i in range(1, 2):
+    # 첫 페이지에 50위, 51위부터는 버튼 한 번 클릭해야 보임
     if i == 50:
         fiftyone_to_hundred = driver.find_element_by_link_text("51 ~ 100 위")
         fiftyone_to_hundred.click()
@@ -95,6 +104,7 @@ for i in range(1, 2):
     else:
         change_songs(i) 
 
+# 닫기
 conn.commit()
 cur.close()
 conn.close()
