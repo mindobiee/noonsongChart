41924ed374a9c0b8941da3d90d937b9b24e3e3f8
김자영 <1476532@sookmyung.ac.kr>
2020-01-25 01:50:36 +0900

    crawling comments

--- comments.py
+++ comments.py
@@ -3,6 +3,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import pymysql 
+import time
+from selenium.webdriver.common.alert import Alert
+from selenium.common.exceptions import NoAlertPresentException
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -10,7 +13,6 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 
 conn = pymysql.connect(host = "127.0.0.1",
                        user = 'root', passwd = '132365', db = 'gradproj')
-cur = conn.cursor()
 
 # options to look like a human
 options = webdriver.ChromeOptions()
@@ -27,46 +29,72 @@ source = driver.page_source
 
 bs = BeautifulSoup(source, 'html.parser')
 
+# 각 곡 상세 페이지에서 댓글 페이지 바꾸는 함수
+# 페이지 옆에 다음 버튼 계속 누름. 마지막 페이지라는 alert 뜰 때까지
 def change_pages():
-    xpath_pages = "/html/body/div[3]/div[2]/div/div/div[5]/div[2]/div[2]/a[5]"
-    button_pages = driver.find_element_by_xpath(xpath_pages)
-    button_pages.click()
+    temp = driver.find_element_by_class_name("page-nav")
+    button_pages = temp.find_element_by_class_name("next")  
 
+    button_pages.click()
+    
+    try:
+        alert = Alert(driver)
+        alert.accept()
+        time.sleep(1)
+
+    except NoAlertPresentException:
+        return True
+
+    return False
+
+# 메인화면에서 곡 선택 하는 함수
+def change_songs(song_num):
+    source = driver.page_source
+    bs = BeautifulSoup(source, 'html.parser')
+    
+    xpath_songs = "html/body/div[3]/div[2]/div[1]/div[6]/div/table/tbody/tr[" + str(song_num) + "]/td[4]/a"
+    button_songs = driver.find_element_by_xpath(xpath_songs)
+    button_songs.click()
+
+    time.sleep(1)
+
+    # crawl page 1
+    crawlComments(bs)
+
+    while(change_pages()):
+        source = driver.page_source
+        bs = BeautifulSoup(source, 'html.parser')
+
+        crawlComments(bs)
+        
+    driver.get('https://genie.co.kr/chart/top200')
     source = driver.page_source
 
-    bs = BeautifulSoup(source, 'html.parser')
+def crawlComments(bs):
+    cur = conn.cursor()
 
-# def change_songs(song_num):
-    
-xpath_songs = "html/body/div[3]/div[2]/div[1]/div[6]/div/table/tbody/tr[" + str(1) + "]/td[4]/a"
-button_songs = driver.find_element_by_xpath(xpath_songs)
-button_songs.click()
-driver.implicitly_wait(5)
+    replies = bs.find_all(class_="reply-text")
 
-source = driver.page_source
-bs = BeautifulSoup(source, 'html.parser')
+    writerId = [ i.find("strong").find("a").get_text() for i in replies ]
+    comment = [ i.find("p").get_text() for i in replies]
 
-replies = bs.find_all(class_="reply-text")
+        # 각 곡별 페이지별 댓글들 DB에 저장
+    for i in range(0, len(writerId)):
+        query = """
+            insert into comments_ (writerId, comment) values ('%s', '%s')
+        """%( str(writerId[i]), str(comment[i]) )
+            
+        try:
+            cur.execute(query)
+        except pymysql.IntegrityError:
+            conn.commit()
+            cur.close()
+            cur = conn.cursor()
 
-writerId = [ i.find("strong").find("a").get_text() for i in replies ]
-comment = [ i.find("p").get_text() for i in replies]
+            continue
 
-# 아이디로 중복제거 해야 함.
-
-
-
-for i in range(0, len(writerId)):
-    query = """
-        insert into comments_ (writerId, comment) values ("%s", "%s")
-    """%(str(writerId[i]), str(comment[i]) )
-    
-    cur.execute(query)
-
-# driver.get('https://genie.co.kr/chart/top200')
-# source = driver.page_source
-
-# for i in range(1, 5):
-#     change_songs(i)
+for i in range(6, 7):
+    change_songs(i)
 
 conn.commit()
 cur.close()
